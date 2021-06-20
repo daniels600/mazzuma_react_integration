b12c0a5517e31bd6e271ae09c48a1f845965df2d
Lorraine Makuyana <58143262+lorrainemakuyana@users.noreply.github.com>
2021-06-20 13:10:30 +0000

    Add code formatting actions

--- src/pages/CheckBalanceFcn.js
+++ src/pages/CheckBalanceFcn.js
@@ -1,28 +1,33 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
 function CheckBalance(props) {
-    const [state, setState ] = useState( {
-        apikey: process.env.REACT_APP_MAZZUMA_API,
-		option: "get_balance"
-    } ); 
+  const [state, setState] = useState({
+    apikey: process.env.REACT_APP_MAZZUMA_API,
+    option: "get_balance",
+  });
 
-    const message = () => {
-        axios
-            .post("https://client.teamcyst.com/phase3/mazexchange-api.php", state )
-            .then((response) => {
-            console.log(response.data);
-            })
-            .catch((error) => console.error(error));
-    }
+  const message = () => {
+    axios
+      .post("https://client.teamcyst.com/phase3/mazexchange-api.php", state)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => console.error(error));
+  };
 
-    return (
-		// const { message } = this.state;
-		<div className="card-body" style={{ display: "block", textAlign: "center" }}>
-			<Button variant="primary" onClick={message}>Click to check balance</Button>
-		</div>
-    ); 
+  return (
+    // const { message } = this.state;
+    <div
+      className="card-body"
+      style={{ display: "block", textAlign: "center" }}
+    >
+      <Button variant="primary" onClick={message}>
+        Click to check balance
+      </Button>
+    </div>
+  );
 }
 
 export default CheckBalance;
