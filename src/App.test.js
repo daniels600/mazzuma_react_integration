ae19b1de9c6cfe69aa9ab84b5168cdb53d335220
Lorraine Makuyana <58143262+lorrainemakuyana@users.noreply.github.com>
2021-06-23 15:56:19 +0000

    Add Homepage Tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+/* test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+}); */
+
+test('sender contact number working', () => {
+  render(<App />);
+  const senderNumber = screen.getByPlaceholderText("Enter sender's number");
+  expect(senderNumber).toBeInTheDocument();
 });
+
+test('recipient contact number working', () => {
+  render(<App />);
+  const recieverNumber = screen.getByPlaceholderText("Enter recipient's number");
+  expect(recieverNumber).toBeInTheDocument();
+});
+
+test('send button clickable', () => {
+  render(<App />);
+  const btnElement = screen.getByTestId("send-button");
+  userEvent.click(btnElement);
+  expect(btnElement).toHaveTextContent(/send/i);
+});
+
--- src/pages/HomepageFcn.js
+++ src/pages/HomepageFcn.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 function HomePage(props) {
 
-    const [state, setState ] = useState({
+    const [state, setState] = useState({
         price: "",
         sender_number: "",
         recipient_number: "",
@@ -11,110 +11,111 @@ function HomePage(props) {
         network: "mtn",
         option: "rmtv",
     });
-    
+
     let orderID = Math.floor(Math.random() * 10000) + 1;
     localStorage.setItem('orderID', orderID);
 
     function changeHandler(e) {
-        setState({ 
-            [e.target.name]: e.target.value 
+        setState({
+            [e.target.name]: e.target.value
         });
     }
-    
+
     function submitHandler(e) {
         e.preventDefault();
         axios
             .post("https://client.teamcyst.com/api_call.php", state)
             .then((response) => {
-            console.log(response.data);
-            setState({ response: response.data });
+                console.log(response.data);
+                setState({ response: response.data });
             })
             .catch((error) => console.error(error));
     }
-    
+
     function message() {
         console.log(this.data)
         axios
             .post("https://client.teamcyst.com/phase3/mazexchange-api.php", this.data)
             .then((response) => {
-            console.log(response.data);
+                console.log(response.data);
             })
             .catch((error) => console.error(error));
     }
 
     const { price, sender_number, recipient_number } = state;
-     
-    return(
-		
-			<div className="card-body" style={{ display: "block", textAlign: "center" }}>
-                <div>
+
+    return (
+
+        <div className="card-body" style={{ display: "block", textAlign: "center" }}>
+            <div>
                 {state.response != null ? (
                     <div
-                    class="alert alert-warning alert-dismissible fade show"
-                    role="alert"
+                        class="alert alert-warning alert-dismissible fade show"
+                        role="alert"
                     >
-                    <h1>{state.response["status"]}</h1>
-                    <button
-                        type="button"
-                        class="close"
-                        data-dismiss="alert"
-                        aria-label="Close"
-                    >
-                        <span aria-hidden="true">&times;</span>
-                    </button>
+                        <h1>{state.response["status"]}</h1>
+                        <button
+                            type="button"
+                            class="close"
+                            data-dismiss="alert"
+                            aria-label="Close"
+                        >
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
                 ) : (
                     <p>
-                    No transaction request sent yet. Kindly send by filling the form
-                    below.
+                        No transaction request sent yet. Kindly send by filling the form
+                        below.
                     </p>
                 )}
-                </div>
-                <form onSubmit={submitHandler}>
-                    <div className="form-group">
-                        <label style={{paddingRight: '40px' }}>Price</label>
-                        <input
+            </div>
+            <form onSubmit={submitHandler}>
+                <div className="form-group">
+                    <label style={{ paddingRight: '40px' }}>Price</label>
+                    <input
                         class="form-control py-4"
                         type="text"
                         placeholder="Enter the price"
                         name="price"
                         value={price}
                         onChange={changeHandler}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label style={{paddingRight: '40px' }}>Sender's contact</label>
-                        <input
+                    />
+                </div>
+                <div className="form-group">
+                    <label style={{ paddingRight: '40px' }}>Sender's contact</label>
+                    <input
                         class="form-control py-4"
                         type="text"
                         placeholder="Enter sender's number"
                         name="sender_number"
                         value={sender_number}
                         onChange={changeHandler}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <label style={{paddingRight: '40px' }}>Recipient's contact</label>
-                        <input
+                    />
+                </div>
+                <div className="form-group">
+                    <label style={{ paddingRight: '40px' }}>Recipient's contact</label>
+                    <input
                         class="form-control py-4"
                         type="text"
                         placeholder="Enter recipient's number"
                         name="recipient_number"
                         value={recipient_number}
                         onChange={changeHandler}
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        name="submit"
-                        className="btn btn-primary btn-lg btn-block"
-                        required
-                    >
-                        Send
-                    </button>
-                </form>
-		    </div>
-		);
+                    />
+                </div>
+                <button
+                    type="submit"
+                    name="submit"
+                    className="btn btn-primary btn-lg btn-block"
+                    data-testid="send-button"
+                    required
+                >
+                    Send
+                </button>
+            </form>
+        </div>
+    );
 }
 
-export default HomePage; 
+export default HomePage;
